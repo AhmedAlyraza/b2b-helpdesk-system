5e3c38535731ce417bf852e1624376544eb1ec83
AhmedAlyraza <ahmedalyraza@gmail.com>
2026-05-04 17:44:46 +0500

    feat: create reusable form field component and refactor login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function LoginPage() {
-    return (
-        <div className="p-10 text-2xl font-semibold">
-            Login Page
-        </div>
-    );
+  return (
+    <div className="w-full max-w-sm space-y-4">
+      <h1 className="text-2xl font-bold">Login</h1>
+
+      <Input placeholder="Email" />
+      <Input placeholder="Password" type="password" />
+
+      <Button className="w-full">Sign In</Button>
+    </div>
+  );
 }
